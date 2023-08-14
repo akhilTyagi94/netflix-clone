4ecff090f98ee274fae563d625e1af9ef50ab48f
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-08-14 23:33:55 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,8 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyByS64sYS7SlC8-tLjqNk0IyEGRt1GRCp4",
-  authDomain: "netflix-clone-bfc52.firebaseapp.com",
-  projectId: "netflix-clone-bfc52",
-  storageBucket: "netflix-clone-bfc52.appspot.com",
-  messagingSenderId: "690499946351",
-  appId: "1:690499946351:web:a10943b8c7d074d67c8a9a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
