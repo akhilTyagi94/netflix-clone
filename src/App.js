fb863bff0bec3f731cd442208de5d70c1de6c961
Akhil Tyagi <tyagi.akhil007@gmail.com>
2023-08-15 10:58:52 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./screens/HomeScreen";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <HomeScreen />
+      <BrowserRouter>
+        <Routes>
+          {!user ? (
+            <Route exact path="/" element={<LoginScreen />} />
+          ) : (
+            <Route exact path="/home" element={<HomeScreen />} />
+          )}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/screens/LoginScreen.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./LoginScreen.css";
+import loginscreenlogo from "../assets/logo.png";
+
+function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
+
+  return (
+    <div className="loginScreen">
+      <div className="loginscreen-background">
+        <img className="loginscreen-logo" src={loginscreenlogo} alt="" />
+        <button onClick={() => setSignIn(true)} className="loginscreen-button">
+          Sign In
+        </button>
+        <div className="loginscreen-gradient" />
+      </div>
+      <div className="loginscreen-body">
+        {signIn ? (
+          <SignInScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, TV programmes and more.</h1>
+            <h2>Watch anywhere. Cancel at any time.</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </h3>
+
+            <div className="loginscreen-input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginscreen-getstarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+export default LoginScreen;
